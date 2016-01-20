84216d27022b04e1dbf1703718d4e324ac44bb01
JordanDx <jordaffix@gmail.com>
2016-01-20 16:03:33 +0100

    Ajout des cristaux et du score. Le joueur gagne des points quand il ramasse des cristaux, le score est affiché.

---
+++ Beat Them All/Assets/Scripts/PointsScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PointsScript : MonoBehaviour {
+
+	private ScoreScript score;
+
+	// Use this for initialization
+	void Start () {
+		score = GameObject.FindGameObjectWithTag ("Player").GetComponent<ScoreScript> ();
+	
+	}
+	
+	void OnTriggerEnter2D(Collider2D target)
+	{
+		if (target.gameObject.tag == "Player")
+		{
+			score.points +=1;
+			Destroy(gameObject);
+
+		}
+	}
+
+}
---
+++ Beat Them All/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+
+	public int points = 0;
+	public GUISkin LabelSkin;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		GUI.skin = LabelSkin;
+		GUI.Label (new Rect (40, 60, 300, 50), "Score : " + points);
+	}
+
+
+
+}
